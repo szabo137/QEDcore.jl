98855bfafcfa8cb7d82f07948ad74cd06e0abcaf
AntonReinhard <anton.reinhard@proton.me>
2024-10-14 12:41:52 +0200

    Add docs for In/Out PhaseSpacePoint & Review comment

--- docs/src/tutorial/particles.jl
+++ docs/src/tutorial/particles.jl
@@ -1,12 +1,13 @@
 # # Particles and Phase Space Points
 
 # There are three layers of abstraction from particles to phase space points in the QEDjl project:
-# - [`AbstractParticleType`](@extref QEDbase.AbstractParticleType): Base type for singleton particle type definitions. We also call these *species*.
-# - [`AbstractParticleStateful`](@extref QEDbase.AbstractParticleStateful): Base type for particles with a direction and carrying a momentum.
-# - [`AbstractPhaseSpacePoint`](@extref QEDbase.AbstractPhaseSpacePoint): Representation of a point in the phase space for a combination of an [`AbstractProcessDefinition`](@extref QEDbase.AbstractProcessDefinition), [`AbstractModelDefinition`](@extref QEDbase.AbstractModelDefinition), and [`AbstractPhasespaceDefinition`](@extref QEDbase.AbstractPhasespaceDefinition).
+# - [`QEDbase.AbstractParticleType`](@extref): Base type for singleton particle type definitions. We also call these *species*.
+# - [`QEDbase.AbstractParticleStateful`](@extref): Base type for particles with a direction and carrying a momentum.
+# - [`QEDbase.AbstractPhaseSpacePoint`](@extref): Representation of a point in the phase space for a combination of an [`QEDbase.AbstractProcessDefinition`](@extref), [`QEDbase.AbstractModelDefinition`](@extref), and [`QEDbase.AbstractPhasespaceDefinition`](@extref).
 
 # This manual is intended to showcase the basic usage of these types and their implementations in QEDcore.
 
+struct UnexpectedSuccess <: Exception end # hide
 using QEDcore
 
 # To use concrete process definitions and models, we also need to use [`QEDprocesses.jl`](https://github.com/QEDjl-project/QEDprocesses.jl)
@@ -21,7 +22,7 @@ using QEDprocesses
 @assert Electron <: Fermion
 @assert Positron <: AntiFermion
 
-# All of these are subtypes of [`AbstractParticleType`](@extref QEDbase.AbstractParticleType).
+# All of these are subtypes of [`QEDbase.AbstractParticleType`](@extref).
 # There are also convenience functions in Julia convention:
 
 @assert is_boson(Photon())
@@ -36,7 +37,7 @@ using QEDprocesses
 
 # ## ParticleStateful
 
-# [`ParticleStateful`](@ref) is the implementation of QEDbase's [`AbstractParticleStateful`](@extref QEDbase.AbstractParticleStateful) interface. It represents a particle with a [`direction`](@extref QEDbase.ParticleDirection) (as used in the context of scattering processes, [`Incoming`](@extref QEDbase.Incoming), [`Outgoing`](@extref QEDbase.Outgoing), or [`UnknownDirection`](@extref QEDbase.UnknownDirection)), a particle species ([`Electron`](@ref), [`Positron`](@ref), [`Photon`](@ref), ...), and a 4-momentum Vector.
+# [`ParticleStateful`](@ref) is the implementation of QEDbase's [`QEDbase.AbstractParticleStateful`](@extref) interface. It represents a particle with a [`direction`](@extref QEDbase.ParticleDirection) (as used in the context of scattering processes, [`QEDbase.Incoming`](@extref), [`QEDbase.Outgoing`](@extref), or [`QEDbase.UnknownDirection`](@extref)), a particle species ([`Electron`](@ref), [`Positron`](@ref), [`Photon`](@ref), ...), and a 4-momentum vector.
 
 ps = ParticleStateful(Incoming(), Electron(), rand(SFourMomentum))
 
@@ -84,7 +85,11 @@ try # hide
         (rand(SFourMomentum),), # incorrect number of incoming momenta, should be 2
         (rand(SFourMomentum), rand(SFourMomentum)),
     )
+    throw(UnexpectedSuccess()) # hide
 catch e # hide
+    if e isa UnexpectedSuccess # hide
+        rethrow(e) # hide
+    end # hide
     @error e # hide
 end # hide
 
@@ -123,7 +128,11 @@ try # hide
             ParticleStateful(Outgoing(), Photon(), rand(SFourMomentum)),
         ),
     )
+    throw(UnexpectedSuccess()) # hide
 catch e # hide
+    if e isa UnexpectedSuccess # hide
+        rethrow(e) # hide
+    end # hide
     @error e # hide
 end # hide
 
@@ -161,7 +170,7 @@ judge(
 # !!! note 
 #     This is only faster when `N` is actually known at compile time, for example when it is a literal integer or a function's type parameter. For dynamic values of `N`, prefer the `Int` variant or in case of loops, directly loop over the tuple of [`momenta`](@extref QEDbase.momenta).
 
-# Some more overloads for the momentum function exist, for a complete list please refer to its documentation: [`momentum`](@extref QEDbase.momentum), [`momenta`](@extref QEDbase.momenta).
+# Some more overloads for the momentum function exist, for a complete list please refer to its documentation: [`QEDbase.momentum`](@extref), [`QEDbase.momenta`](@extref).
 
 # Finally, [`process`](@ref), [`model`](@ref), and [`phase_space_definition`](@ref) can be used to request the object in question:
 
@@ -170,3 +179,52 @@ process(psp)
 model(psp)
 #
 phase_space_definition(psp)
+
+# ## In/Out Phase Space Points
+
+# As a special case, phase space points are allowed to only contain the incoming or outgoing particle momenta.
+# These types can be helpful for overloading some functions that don't require the entire phase space point to exist.
+
+function in_sum(in_psp::AbstractInPhaseSpacePoint)
+    return sum(momenta(in_psp, Incoming()))
+end
+
+psp = InPhaseSpacePoint(
+    Compton(),
+    PerturbativeQED(),
+    PhasespaceDefinition(SphericalCoordinateSystem(), ElectronRestFrame()),
+    (rand(SFourMomentum), rand(SFourMomentum)),
+)
+
+in_sum(psp)
+
+# Every full [`PhaseSpacePoint`](@ref) is both an [`InPhaseSpacePoint`](@ref) and an [`OutPhaseSpacePoint`](@ref), too. For example, the `in_sum` function defined above still works with a full [`PhaseSpacePoint`](@ref):
+
+psp = PhaseSpacePoint(
+    Compton(),
+    PerturbativeQED(),
+    PhasespaceDefinition(SphericalCoordinateSystem(), ElectronRestFrame()),
+    (rand(SFourMomentum), rand(SFourMomentum)),
+    (rand(SFourMomentum), rand(SFourMomentum)),
+)
+
+in_sum(psp)
+
+# But an [`InPhaseSpacePoint`](@ref) is not an [`OutPhaseSpacePoint`](@ref) and vice versa. We cannot call `in_sum` on an [`OutPhaseSpacePoint`](@ref):
+
+psp = OutPhaseSpacePoint(
+    Compton(),
+    PerturbativeQED(),
+    PhasespaceDefinition(SphericalCoordinateSystem(), ElectronRestFrame()),
+    (rand(SFourMomentum), rand(SFourMomentum)),
+)
+
+try # hide
+    in_sum(psp)
+    throw(UnexpectedSuccess()) # hide
+catch e # hide
+    if e isa UnexpectedSuccess # hide
+        rethrow(e) # hide
+    end # hide
+    @error e # hide
+end # hide
